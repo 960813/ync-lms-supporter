fd6619d1ce993a739be942238b25817c1d5a8a37
Keriel <keriel@jupiterflow.com>
2020-09-05 17:20:40 +0900

    :bug: LMS에 이메일 인증이 되어 있지 않는 경우 발생하는 버그 수정
    * 아예 등록되지 않은 경우는 추후 수정 예정

--- src/popup.js
+++ src/popup.js
@@ -73,8 +73,8 @@ const getUserInfo = () => {
                 const user_name_match = profile_data.match(/<title>사용자설정:(.*)\((.*)\)<\/title>/);
                 const user_name = user_name_match[1] + '(' + encodeURIComponent(user_name_match[2] + ' )');
 
-                const user_email = encodeURIComponent(profile_data.match(/<pclass=\"pathemail_channelcontact_channel_pathellipsis\">(.*)<\/>/)[1]);
-
+                // const user_email = encodeURIComponent(profile_data.match(/<pclass=\"pathemail_channelcontact_channel_pathellipsis\">(.*)<\/>/)[1]);
+                const user_email = encodeURIComponent(profile_data.match(/"primary_email":"(.*)","log/)[1]);
 
                 const option_data = profile_data.match(/id="profile_pseudonym_id"><optionvalue="(\d*)">(\d*)<\/option><\/select>/);
 
@@ -184,9 +184,6 @@ document.addEventListener('DOMContentLoaded', function () {
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     <title>YNC LMS Supporter</title>
     <style>
     .table > tbody > tr > td {
